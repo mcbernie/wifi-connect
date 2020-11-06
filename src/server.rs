18ecb5c02de906e7dd3c5dd42855104cc0134165
Nicolas Brüggemann (mcbernie) <nb@zweiund40.com>
2020-11-06 11:31:26 +0100

    more dhcp settings for captive portal dedection

--- src/server.rs
+++ src/server.rs
@@ -108,8 +108,6 @@ struct RedirectMiddleware;
 impl AfterMiddleware for RedirectMiddleware {
     fn catch(&self, req: &mut Request, err: IronError) -> IronResult<Response> {
         
-        warn!("make a catch on the request {:?}", req);
-        
         let gateway = {
             let request_state = get_request_state!(req);
             format!("{}", request_state.gateway)
