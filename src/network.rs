d047294c6bfa0d84438fced53864aa015c94360d
Nicolas Brüggemann (mcbernie) <nb@zweiund40.com>
2021-06-16 11:58:28 +0200

    using iw and hostapd: no 9

--- src/network.rs
+++ src/network.rs
@@ -97,9 +97,9 @@ impl NetworkCommandHandler {
         };
 
         create_phy_if(config);
-        thread::sleep(Duration::from_sec(1));
+        thread::sleep(Duration::from_secs(1));
         let hostapd = start_hostapd(config)?;
-        thread::sleep(Duration::from_sec(1));
+        thread::sleep(Duration::from_secs(1));
         let dnsmasq = start_dnsmasq(config)?;
 
         let (server_tx, server_rx) = channel();
